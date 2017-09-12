d5e044ecd7c9e9198b6f1831f7a181b8d09fb8ae
Star Wars <31880419+Mk-Team@users.noreply.github.com>
2017-09-12 11:10:37 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# StarWarsAr
+# [<< TH3BOSS >> V17 (Final Version)](https://telegram.me/llDEV1ll)
+
+**An advanced and powerful administration bot based on NEW TG-CLI
+
+
+* * *
+
+
+# Installation
+
+
+```sh
+<< TH3BOSS >>
+
+🔸➖🔹➖🔸➖🔹
+<< V17 WE ARE TH3BEST >>
+🔸➖🔹➖🔸➖🔹
+ << كـيـفـيـه تـنـصـيـب سـورس الـزعـيـم >>
+🔸➖🔹➖🔸➖🔹
+ << افتـح ترمنـــأل وخلي >>
+🔸➖🔹➖🔸➖🔹
+# Let's Install The Bot >>
+🔸➖🔹➖🔸➖🔹
+cd $HOME
+🔸➖🔹➖🔸➖🔹
+git clone https://github.com/moody2020/TH3BOSS.git
+🔸➖🔹➖🔸➖🔹
+cd TH3BOSS
+🔸➖🔹➖🔸➖🔹
+chmod +x TH3BOSS.sh
+🔸➖🔹➖🔸➖🔹
+./TH3BOSS.sh install
+🔸➖🔹➖🔸➖🔹
+./TH3BOSS.sh 
+🔸➖🔹➖🔸➖🔹
+# Enter A Phone Number & Confirmation Code >>
+🔸➖🔹➖🔸➖🔹
+# For Auto Launch >>
+🔸➖🔹➖🔸➖🔹
+cd TH3BOSS
+🔸➖🔹➖🔸➖🔹
+chmod 777 autobd.sh
+🔸➖🔹➖🔸➖🔹
+screen ./autobd.sh
+🔸➖🔹➖🔸➖🔹
+<< TH3BOSS >>
+
+<< # Dev Bot @TH3BOSS >>
+
+<< # groupmanger : @lBOSSl  >>
+```
+### One command
+To install everything in one command, use:
+```sh
+cd $HOME && git clone https://github.com/moody2020/TH3BOSS.git && cd TH3BOSS && chmod +x TH3BOSS.sh && ./TH3BOSS.sh install && ./TH3BOSS.sh
+
+OR
+
+cd $HOME && git clone https://github.com/moody2020/TH3BOSS.git && cd TH3BOSS && chmod +x TH3BOSS.sh && ./TH3BOSS.sh install && chmod 777 autobd.sh && screen ./autobd.sh
+```
+
+* * *
+
+# Support and Development
+
+More information [TH3BOSS Global Chat](https://t.me/joinchat/A5_fO0OPsgbb_99AkUgWQ)
+
+More information [TH3BOSS Channel](https://t.me/lBOSSl)
+
+
+* * *
+
+# Developers !
+
+[TH3BOSS](https://github.com/moody2020) ([Telegram](https://telegram.me/TH3BOSS))
+
+[Mohammed Hisham](https://github.com/moody2020) ([Telegram](https://telegram.me/lBOSSl))
+
+[TEAMBOSS](https://github.com/moody2020) ([Telegram](https://telegram.me/lBOSSl))
+
+
+### Our Telegram channel : [@llDEV1ll](https://telegram.me/llDEV1ll)
+
+# Special thanks to Omar Alsaray : [@blcon](https://telegram.me/blcon)
+
+# [<< TH3BOSS >> V17 (Final Version)](https://telegram.me/llDEV1ll)
---
+++ TH3BOSS.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+install() {
+	    cd tg
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+		wget https://valtman.name/files/telegram-cli-1222
+		mv telegram-cli-1222 tgcli
+		chmod +x tgcli
+		cd ..
+		chmod +x bot
+		chmod +x tg
+		chmod +x autobd.sh
+}
+
+function print_logo() {
+	green " "
+	green "        -- __  __                    _       
+                   --|  \/  | __ _ _ __ ___ ___| | ___  
+                   --| |\/| |/ _` | '__/ __/ _ \ |/ _ \ 
+                   --| |  | | (_| | | | (_|  __/ | (_) |
+                   --|_|  |_|\__,_|_|  \___\___|_|\___/ 
+                   -- BY :- @iiDii Ch :- @Star_Wars"
+	green " "
+	green " "
+	echo -e "\n\e[0m"
+}
+
+function logo_play() {
+    declare -A txtlogo
+    seconds="0.010"
+    txtlogo[1]=" "
+    txtlogo[2]="  -- __  __                    _       
+                  --|  \/  | __ _ _ __ ___ ___| | ___  
+                  --| |\/| |/ _` | '__/ __/ _ \ |/ _ \ 
+                  --| |  | | (_| | | | (_|  __/ | (_) |
+                  --|_|  |_|\__,_|_|  \___\___|_|\___/ 
+                  -- BY :- @iiDii Ch :- @Star_Wars "
+    txtlogo[3]=" "
+    txtlogo[4]=" "
+    printf "\e[31m\t"
+    for i in ${!txtlogo[@]}; do
+        for x in `seq 0 ${#txtlogo[$i]}`; do
+            printf "${txtlogo[$i]:$x:1}"
+            sleep $seconds
+        done
+        printf "\n\t"
+    done
+    printf "\n"
+	echo -e "\e[0m"
+}
+
+function th3boss() {
+	echo -e "\e[0m"
+	  red "     >>>>                         We Are Not Attacker                             "
+    green "     >>>>                         We Are Not Alliance                             "
+	white "     >>>>                         We Are Programmer                               "
+	white "     >>>>                         We Are The Best                                 "
+	  red "     >>>>                         We Are Family                                   "
+	  red "     >>>>                         @Star_Wars                                      "
+	echo -e "\e[0m"
+}
+
+red() {
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+}
+green() {
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+}
+white() {
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+}
+update() {
+	git pull
+}
+
+if [ "$1" = "install" ]; then
+	print_logo
+	th3boss
+	logo_play
+	install
+  else
+if [ ! -f ./tg/tgcli ]; then
+    echo "tg not found"
+    echo "Run $0 install"
+    exit 1
+ fi
+	print_logo
+	th3boss
+	logo_play
+   #sudo service redis-server restart
+   ./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
+   #./tg/tgcli -s ./bot/bot.lua $@
+fi
